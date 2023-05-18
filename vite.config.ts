fd0c379c132a94d397859365b7cc0ace9e581a7a
Mehdi <birkyo@gmail.com>
2023-05-18 17:34:03 +0200

    v1 some refacto

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith("ion-"),
-        },
-      },
-    }),
-  ],
+  base: "/",
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
