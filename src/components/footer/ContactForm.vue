6f827fa1868c9856589b203e749f954068cfe1c5
Mehdi <birkyo@gmail.com>
2023-05-18 12:19:38 +0200

    contact from wip

--- src/components/footer/ContactForm.vue
+++ src/components/footer/ContactForm.vue
@@ -1,15 +1,25 @@
 <template>
   <section>
-    <form>
-      <input type="text" v-for="input in inputs" />
+    <form :submit="sendMessage(name, email, message)">
+      <h3>CONTACT</h3>
+      <input type="text" placeholder="NAME / NOM *" v-model="name" required />
+      <input type="email" placeholder="EMAIL *" v-model="email" required />
+      <textarea placeholder="MESSAGE *" v-model="message" required></textarea>
+      <button type="submit">SEND MESSAGE</button>
     </form>
-    <textarea name="" id="" cols="30" rows="10"></textarea>
   </section>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { ref, computed } from "vue";
 
+const name = ref("");
+const email = ref("");
+const message = ref("");
+
+const sendMessage = (name: string, email: string, message: string) => {
+  console.log(name, email, message);
+};
 const inputs = computed(() => {
   return 4;
 });
@@ -17,24 +27,50 @@ const inputs = computed(() => {
 
 <style scoped lang="scss">
 section {
+  @include fdCol-jcCt-aiCt;
   min-height: 500px;
   width: 100%;
-  background-color: #ffffff;
-  padding: 50px 2px;
-}
-
-.test {
-  width: 300px;
-  height: 400px;
-  background-color: black;
-  color: white;
+  padding: 70px 20px;
+  h3 {
+    width: 100%;
+    font-size: 3rem;
+    color: rgb(34, 32, 32);
+  }
 }
 
 form {
-  input {
-    width: 50%;
-    height: 50px;
+  display: flex;
+  column-gap: 10px;
+  flex-wrap: wrap;
+  max-width: 900px;
+  height: 400px;
+  color: white;
+  & input,
+  textarea {
     border: 1px solid black;
+    outline: none;
+    padding: 1em;
+    &::placeholder {
+      color: black;
+      font-size: 1.2em;
+    }
+  }
+  & input {
+    flex: 1;
+    height: 50px;
+  }
+  & textarea {
+    width: 100%;
+    height: 150px;
+  }
+  & button {
+    width: 200px;
+    height: 60px;
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: transparent;
+    border: 1px solid #212529;
+    cursor: pointer;
   }
 }
 </style>
