576b49d2397881fdc5655a01963deddb04e43448
Mehdi <birkyo@gmail.com>
2023-06-03 16:45:59 +0200

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,7 @@ export default defineConfig({
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_PROD_DEVTOOLS__: false,
   },
+  build: {
+    manifest: true, // Permet de générer un manifest.json
+  },
 });
